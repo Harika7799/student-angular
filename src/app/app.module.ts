81746ffbf264d8121a36f8a14c93e510ac51fce4
001ZNC744 <addalaharika9009@gmail.com>
2021-07-27 15:09:18 +0530

    student

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {StudentProjectListComponent} from "./student-project-list/student-project-list.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentProjectListComponent
   ],
   imports: [
     BrowserModule
